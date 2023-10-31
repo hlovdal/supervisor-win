69874bc2e2256ef110fe17dfd28d7fe26e0ede81
alexsilva <geniofuturo@gmail.com>
2023-10-31 17:37:22 -0300

    Adding 'time_machine' function.

--- supervisor/utils.py
+++ supervisor/utils.py
@@ -1,4 +1,5 @@
 from functools import wraps
+import time
 
 
 def raise_not_implemented(obj):
@@ -9,3 +10,18 @@ def raise_not_implemented(obj):
 		raise NotImplementedError("{0.__name__} '{1.__name__}' not implemented!".format(type(obj), obj))
 
 	return __inner
+
+def time_machine(frame_time):
+	"""Create a frame timer"""
+	last_frame = time.time()
+
+	while True:
+		current_time = time.time()
+		delta_time = current_time - last_frame
+
+		if delta_time < frame_time:
+			time_to_sleep = frame_time - delta_time
+			time.sleep(time_to_sleep)
+
+		last_frame = time.time()
+		yield
