29abfb86fdd6f393844ec0c863a3e9ee11f55aae
alexsilva <geniofuturo@gmail.com>
2023-10-31 17:35:00 -0300

    reformat.

--- supervisor/utils.py
+++ supervisor/utils.py
@@ -2,10 +2,10 @@ from functools import wraps
 
 
 def raise_not_implemented(obj):
-    """Raise NotImplementedError"""
-    @wraps(obj)
-    def __inner(*args, **kwargs):
-        raise NotImplementedError(
-            "{0.__name__} '{1.__name__}' not implemented!".format(
-                type(obj), obj))
-    return __inner
+	"""Raise NotImplementedError"""
+
+	@wraps(obj)
+	def __inner(*args, **kwargs):
+		raise NotImplementedError("{0.__name__} '{1.__name__}' not implemented!".format(type(obj), obj))
+
+	return __inner
